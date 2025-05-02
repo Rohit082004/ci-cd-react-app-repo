346cfea5e021159f515c88c07ff3e7e5a5287c51
Rohit Mahajan <mahajanrohit08.07@gmail.com>
2025-05-02 16:52:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>My react app</h1>
+      <h2>my react impliment with CI/CD</h2>
+      <h3>with Github Action</h3>
+    </div>
+  )
+}
+
+export default App
